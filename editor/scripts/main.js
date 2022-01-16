db72d3073edafbc3913bad66a28691972f4181b7
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-01-16 11:03:22 -0300

    up

--- editor/scripts/main.js
+++ editor/scripts/main.js
@@ -24,20 +24,100 @@ function OnEnterMenu(indice)
 {
     let modal = modalWindows[indice];
     modalPosX = modal.parentElement.style.left-10;
-    modal.style.display = "block";
+    modalWindows[indice].classList.remove("hidden");
 }
 function OnExitMenu(indice)
 {
-    modalWindows[indice].style.display = "none";
+    modalWindows[indice].classList.add("hidden");
 }
+
+var map;
+function LoadMap (){
+    const loadedMap = JSON.parse(this.responseText);
+    console.log(loadedMap);
+    map = loadedMap;
+    const maptitle = "map"+loadedMap.mapname+".json";
+    const titleEl = document.getElementById("arquivo-title");
+    const mapfile = document.getElementById("map-file");
+    const aba = mapfile.querySelector(".documento__aba");
+
+    const tilesheetName = document.getElementById("tilesheets_name");
+    const tilesheetBox = document.getElementById("tilesheets_box");
+    
+    CreateSpriteOptions(tilesheetName, loadedMap.tilesheet);
+
+    titleEl.innerText = maptitle;
+    aba.children[0].innerText = maptitle;
+    aba.children[1].onclick = () => 
+    {
+        mapfile.classList.add("hidden");
+        titleEl.innerText = "";
+        window.location.search = "";
+    };
+
+    mapfile.classList.remove("hidden");
+}
+
+function LoadTSheet (){
+    const loadedMap = JSON.parse(this.responseText);
+    console.log(loadedMap);
+    map = loadedMap;
+    const maptitle = "tilesheet" + loadedMap.name + ".json";
+    const titleEl = document.getElementById("arquivo-title");
+    const mapfile = document.getElementById("map-file");
+    const aba = mapfile.querySelector(".documento__aba");
+
+    const tilesheetName = document.getElementById("tilesheets_name");
+    const tilesheetBox = document.getElementById("tilesheets_box");
+    
+    CreateSpriteOptions(tilesheetName, loadedMap.tilesheet);
+
+    titleEl.innerText = maptitle;
+    aba.children[0].innerText = maptitle;
+    aba.children[1].onclick = () => 
+    {
+        mapfile.classList.add("hidden");
+        titleEl.innerText = "";
+        window.location.search = "";
+    };
+
+    mapfile.classList.remove("hidden");
+}
+
+function LoadParams () 
+{
+    const urlparams = new URLSearchParams(window.location.search);
+    const tilemap = urlparams.get("input-tilemap");
+    if(tilemap != undefined){
+        const oReq = new XMLHttpRequest();
+        oReq.onload = LoadMap;
+        
+        oReq.open('POST', '/editor/loadmap');
+        oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        oReq.send("mapname="+tilemap);
+    }
+
+    const tilesheet = urlparams.get("input-tilesheet");
+    if(tilesheet != undefined){
+        const oReq = new XMLHttpRequest();
+        oReq.onload = LoadTSheet;
+        
+        oReq.open('POST', '/editor/loadtilesheet');
+        oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        oReq.send("name="+tilesheet);
+    }
+
+    //console.log(tilemap);
+}
+LoadParams ();
 
 var menuSelections = 
 [
     [
-        WindowCreateMapa,
-        evt => {},
-        evt => {},
-        evt => {},
+        MenuTilemapCreate,
+        MenuTilemapLoad,
+        MenuTilesheetCreate,
+        MenuTilesheetLoad,
     ]
 ];
 
@@ -48,19 +128,139 @@ function menuSelect (i, j)
 
 //#endregion
 //#region Mapa
-var map = document.createElement("mapa");
-map.id = "mapa";
 
-function WindowCreateMapa (evt)
+function CreateOption (title, value)
 {
-    console.log("Clicou");
-    
+    const el = document.createElement("option");
+    el.textContent = title;
+    el.value = value;
+    return el;
+}
+
+function LoadSpritesheets (callback){
+    const oReq = new XMLHttpRequest();
+    oReq.onload = evt => {
+        const t = JSON.parse(evt.target.responseText);
+        const tilesheets = t.reduce ((p, o) =>
+        {
+            const substrings = o.split(".");
+            if(substrings[1] != "json") return p;
+            p.push({name:substrings[0], type:substrings[1]});
+            return p;
+        }, []);
+        callback(tilesheets);
+    };
+    oReq.open('GET', "/tilesheets");
+    oReq.send();
+}
+
+function CreateSpriteOptions (selectEl, defaultValue) {
+    LoadSpritesheets(ts => 
+    {
+        selectEl.innerHTML = "";
+        for (let i = 0; i < ts.length; i++)
+        {
+            const o = ts[i];
+            const v =  o.name+"."+o.type;
+            
+            selectEl.append(CreateOption(o.name, v));
+            if(defaultValue == v) selectEl.selectedIndex = i;
+        }
+    });   
+}
+
+function MenuTilemapCreate ()
+{   
     let popupWindow = document.createElement("div");
     popupWindow.classList.add("menu__windowbox");
     LoadHTML("mapcreation.html", x => 
     {
+        CreateSpriteOptions(x.querySelector("select"), undefined);
+
         x.querySelector("#cancelbutton").onclick = x => {CancelCreateMapa(popupWindow);};
-        //x.querySelector("#createbutton").onclick = x => {FinishCreateMapa(popupWindow);};
+        popupWindow.append(x);
+    });
+    body.insertBefore(popupWindow, body.firstChild);
+    //mapcreation.tabIndex = 0;
+}
+
+function MenuTilemapLoad ()
+{   
+    let popupWindow = document.createElement("div");
+    popupWindow.classList.add("menu__windowbox");
+    LoadHTML("maploading.html", x => 
+    {
+        const oReq = new XMLHttpRequest();
+        oReq.onload = evt => {
+            const t = JSON.parse(evt.target.responseText);
+            const tsBox = x.querySelector("select");
+            tsBox.innerHTML = "";
+            const mapNames = t.reduce ((p, o) =>
+            {
+                const substrings = o.split(".");
+                if(substrings[1] != "json") return p;
+                p.push({name:substrings[0], type:substrings[1]});
+                return p;
+            }, []);
+            for (let i = 0; i < mapNames.length; i++)
+            {
+                const o = mapNames[i];
+                //console.log(o);
+                tsBox.append(CreateOption(o.name, o.name+"."+o.type));
+            }
+        };
+        oReq.open('GET', "/tilemaps");
+        oReq.send();
+
+        x.querySelector("#cancelbutton").onclick = x => {CancelCreateMapa(popupWindow);};
+        popupWindow.append(x);
+    });
+    body.insertBefore(popupWindow, body.firstChild);
+    //mapcreation.tabIndex = 0;
+}
+
+function MenuTilesheetCreate ()
+{   
+    let popupWindow = document.createElement("div");
+    popupWindow.classList.add("menu__windowbox");
+    LoadHTML("tilesheetcreation.html", x => 
+    {
+        x.querySelector("#cancelbutton").onclick = x => {CancelCreateMapa(popupWindow);};
+        popupWindow.append(x);
+    });
+    body.insertBefore(popupWindow, body.firstChild);
+    //mapcreation.tabIndex = 0;
+}
+
+function MenuTilesheetLoad ()
+{   
+    let popupWindow = document.createElement("div");
+    popupWindow.classList.add("menu__windowbox");
+    LoadHTML("tilesheetloading.html", x => 
+    {
+        const oReq = new XMLHttpRequest();
+        oReq.onload = evt => {
+            const t = JSON.parse(evt.target.responseText);
+            const tsBox = x.querySelector("select");
+            tsBox.innerHTML = "";
+            const mapNames = t.reduce ((p, o) =>
+            {
+                const substrings = o.split(".");
+                if(substrings[1] != "json") return p;
+                p.push({name:substrings[0], type:substrings[1]});
+                return p;
+            }, []);
+            for (let i = 0; i < mapNames.length; i++)
+            {
+                const o = mapNames[i];
+                //console.log(o);
+                tsBox.append(CreateOption(o.name, o.name+"."+o.type));
+            }
+        };
+        oReq.open('GET', "/tilesheets");
+        oReq.send();
+
+        x.querySelector("#cancelbutton").onclick = x => {CancelCreateMapa(popupWindow);};
         popupWindow.append(x);
     });
     body.insertBefore(popupWindow, body.firstChild);
